b096f54fe92ccd2eb410044485df3592827545c0
Andrea <andreamontereale@gmail.com>
2023-05-16 17:00:39 +0200

    Merge fix

---
+++ DragonsFaith/Assets/Scripts/Inventory/Items/ConsumableWeapon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Inventory.Items
+{
+    [CreateAssetMenu(menuName = "ItemsType/ConsumableWeapon")]
+    public class ConsumableWeapon : Weapon
+    {
+        public ConsumableWeapon()
+        {
+            type = ItemType.Weapon;
+            consumable = true;
+            stackable = true;
+        }
+
+        public override void PerformAction()
+        {
+            base.PerformAction();
+            
+        }
+    }
+}
